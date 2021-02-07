2ee729e44da5ab3e80d80acd857ffb6ae443a111
Andoni <75397542+aferpe@users.noreply.github.com>
2021-02-07 17:37:09 +0100

    Update invidious-cli.sh
    Switched to an Invidious instance which doesn't block its API

--- invidious-cli.sh
+++ invidious-cli.sh
@@ -2,14 +2,14 @@
 echo "Enter your search query"
 read query
 query_s=$(echo "$query" | sed 's:\s:+:g')
-video=$(curl "https://invidious.kavin.rocks/api/v1/search/?q=$query_s" -s --retry-all-errors)
+video=$(curl "https://invidious.snopyta.org/api/v1/search/?q=$query_s" -s --retry-all-errors)
 list=$(echo "$video" | grep -o -P '"title":"(.+?)","videoId"' | sed s/'"title":"'// | sed s/'","videoId"'// | sed 's:\\::g' | nl -b a)
 echo "$list"
 echo -e "\nEnter the number of the video you want to watch"
 while :; do
 read number
 video_id=$(echo $video | grep -o -E '"videoId":"[^"]+"' | sed -n ''''$number'p''' | sed 's/"videoId"://' | sed 's/"//')
-mpv https://invidious.kavin.rocks/watch?v=$video_id --fs 
+mpv https://invidious.snopyta.org/watch?v=$video_id --fs 
 clear
 echo "$list"
 echo -e "\nEnter another number, or quit by pressing Ctrl+C"
